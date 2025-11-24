92495e2db75c6d1a3c1e3c57a5f3d55663f66bb4
malki23 <52123542+malki23@users.noreply.github.com>
2025-11-24 23:50:42 +0200

    4.0

--- MyString.java
+++ MyString.java
@@ -38,8 +38,10 @@ public class MyString {
         for (int i = 0; i < str1.length(); i++) {
             if ((str1.charAt(i)) == str2.charAt(0)) {
                 for (int j = 0; j < str2.length(); j++) {
-                    if ((str1.charAt(i + j)) == str2.charAt(j)) {
-                        count++;
+                    if (i + j < str1.length()) {
+                        if ((str1.charAt(i + j)) == str2.charAt(j)) {
+                            count++;
+                        }
                     }
                 }
                 if (count == str2.length()) {
